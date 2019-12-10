9a733b97a77e61ca2611f9bf6f66f0b3734bf9aa
severinsimmler <severin.simmler@posteo.de>
2019-12-10 01:57:26 +0100

    refactor: annotation

--- src/app/models/testcase.ts
+++ src/app/models/testcase.ts
@@ -1,67 +1,67 @@
-// https://github.com/weichx/cerialize
+import { serializeAs } from 'cerialize';
 
-export class Testcase {
-    tokens: Array<Token>;
-    isAnnotated: boolean;
-    isValid: boolean;
 
-    constructor(tokens: Array<string>) {
-        this.tokens = tokens.map(token => new Token(token));
-    }
-}
-
-export class Token {
-    text: string;
-    namedEntities: NamedEntities;
-    relations: Relations;
-
-    constructor(token: string) {
-        this.text = token;
-        this.namedEntities = new NamedEntities();
-        this.relations = new Relations();
-    }
-}
-
-class Relations {
-    isProperty: boolean;
-    isProfessionalPosition: boolean;
-    isKinship: boolean;
-    relatesTo: Array<number> | null;
-    relatesAs: Array<number> | null;
+class Attributes {
+    @serializeAs('is_title') isTitle: boolean;
+    @serializeAs('is_position') isPosition: boolean;
+    @serializeAs('is_kinship') isKinship: boolean;
+    @serializeAs('is_property') isProperty: boolean;
 
     constructor(
-        isProperty: boolean = false,
-        isProfessionalPosition: boolean = false,
+        isTitle: boolean = false,
+        isPosition: boolean = false,
         isKinship: boolean = false,
-        relatesTo: Array<number> | null = null,
-        relatesAs: Array<number> | null = null
-    ) {
-        this.isProperty = isProperty;
-        this.isProfessionalPosition = isProfessionalPosition;
+        isProperty: boolean = false
+        ) {
+        this.isTitle = isTitle;
+        this.isPosition = isPosition;
         this.isKinship = isKinship;
-        this.relatesTo = relatesTo;
-        this.relatesAs = relatesAs;
+        this.isProperty = isProperty;
     }
 }
 
 class NamedEntities {
-    isTitle: boolean;
-    isFirstName: boolean;
-    isLastName: boolean;
-    isPronoun: boolean;
-    isMisc: boolean;
+    @serializeAs('is_first_name') isFirstName: boolean;
+    @serializeAs('is_last_name') isLastName: boolean;
+    @serializeAs('is_organization') isOrganization: boolean;
+    @serializeAs('is_appellativum') isAppellativum: boolean;
 
     constructor(
-        isTitle: boolean = false,
         isFirstName: boolean = false,
         isLastName: boolean = false,
-        isPronoun: boolean = false,
-        isMisc: boolean = false
+        isOrganization: boolean = false,
+        isAppellativum: boolean = false
     ) {
-        this.isTitle = isTitle;
         this.isFirstName = isFirstName;
         this.isLastName = isLastName;
-        this.isPronoun = isPronoun;
-        this.isMisc = isMisc;
+        this.isOrganization = isOrganization;
+        this.isAppellativum = isAppellativum;
+    }
+}
+
+export class Token {
+    @serializeAs('text') text: string;
+    @serializeAs('identifier') identifier: string | null;
+    @serializeAs(NamedEntities, 'named_entities') namedEntities: NamedEntities;
+    @serializeAs(Attributes, 'attributes') attributes: Attributes;
+
+    constructor(token: string) {
+        this.text = token;
+        this.identifier = null;
+        this.namedEntities = new NamedEntities();
+        this.attributes = new Attributes();
+    }
+}
+
+export class Testcase {
+    @serializeAs('id') id: number;
+    @serializeAs('tokens') tokens: Array<Token>;
+    @serializeAs('is_annotated') isAnnotated: boolean;
+    @serializeAs('is_valid') isValid: boolean;
+
+    constructor(tokens: Array<string>) {
+        this.tokens = tokens.map(token => new Token(token));
+        this.isAnnotated = false;
+        this.isValid = true;
     }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { Testcase, Token, KnowledgeBase } from '../../models';
 
+import { Serialize } from 'cerialize';
+
 // https://stackblitz.com/edit/httpsstackoverflowcomquestions51806464how-to-create-and-downloa?file=src/app/app.component.ts
 
 
@@ -12,13 +14,80 @@ import { Testcase, Token, KnowledgeBase } from '../../models';
 })
 export class HomeComponent {
   testcase: Testcase;
+  selectedLabel: string;
   selectedToken: Token;
-  knowledgeBase: KnowledgeBase;
 
   constructor() {
-    this.testcase = new Testcase(['Nice', 'Cool', 'Mann', 'Cool', '.']);
-    this.selectedToken = this.testcase.tokens[0];
-    this.knowledgeBase = {ok: 123};
+    let tokens = ['Dr', '.', 'Christian', 'Göke', ',', 'Geschäftsführer', 'der', 'Messe', 'Berlin', 'GmbH', ':', '"', 'Argentinien', 'mit', 'seiner', 'breiten', 'Angebotspalette', 'ist', 'ein', 'bedeutender', 'Obst', '-', 'und', 'Gemüseproduzent', ',', 'der', 'seine', 'Erzeugnisse', 'in', 'über', '60', 'Länder', 'exportiert', '.'];
+    this.testcase = new Testcase(tokens);
+    this.selectedLabel = 'firstName';
+    this.selectedToken = this.testcase[0];
+  }
+
+  public assignLabel(token: Token) {
+    if (this.selectedLabel === 'firstName') {
+      if (token.namedEntities.isFirstName) {
+        token.namedEntities.isFirstName = false;
+      } else {
+        token.namedEntities.isFirstName = true;
+      }
+    }
+
+    if (this.selectedLabel === 'lastName') {
+      if (token.namedEntities.isLastName) {
+        token.namedEntities.isLastName = false;
+      } else {
+        token.namedEntities.isLastName = true;
+      }
+    }
+
+    if (this.selectedLabel === 'appellativum') {
+      if (token.namedEntities.isAppellativum) {
+        token.namedEntities.isAppellativum = false;
+      } else {
+        token.namedEntities.isAppellativum = true;
+      }
+    }
+
+    if (this.selectedLabel === 'organization') {
+      if (token.namedEntities.isOrganization) {
+        token.namedEntities.isOrganization = false;
+      } else {
+        token.namedEntities.isOrganization = true;
+      }
+    }
+
+    if (this.selectedLabel === 'title') {
+      if (token.attributes.isTitle) {
+        token.attributes.isTitle = false;
+      } else {
+        token.attributes.isTitle = true;
+      }
+    }
+
+    if (this.selectedLabel === 'position') {
+      if (token.attributes.isPosition) {
+        token.attributes.isPosition = false;
+      } else {
+        token.attributes.isPosition = true;
+      }
+    }
+
+    if (this.selectedLabel === 'kinship') {
+      if (token.attributes.isKinship) {
+        token.attributes.isKinship = false;
+      } else {
+        token.attributes.isKinship = true;
+      }
+    }
+
+    if (this.selectedLabel === 'property') {
+      if (token.attributes.isProperty) {
+        token.attributes.isProperty = false;
+      } else {
+        token.attributes.isProperty = true;
+      }
+    }
   }
 
 }
